1a0123be7c749e671e180d9d2be62210872eeb70
Parul Pachori <ppachori@usxpress.com>
2025-07-01 02:49:32 +0530

    test changes

--- rpConfigCi.js
+++ rpConfigCi.js
@@ -2,8 +2,8 @@ const config = {
   // required fields
   apiKey: 'DX-Test_rGPk9D3wT9Ky2r4c_Yn7PulzV2UR-5C25aQ1byvuWtAByT3x3XnLm6njfmLa8NMo',
   endpoint: 'https://reportportal.dpl.usxpress.io/api/v1',
-  project: 'default_personal',
-  launch: 'dev_geeks-for-geeks',
+  project: 'developmemnt',
+  launch: 'orders',
   // optional fields
   attributes: [
     {
