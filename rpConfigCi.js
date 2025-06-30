3b5942778a09822d1dbf4c9872686826c8aca008
Parul Pachori <ppachori@usxpress.com>
2025-07-01 00:15:41 +0530

    test changes

--- rpConfigCi.js
+++ rpConfigCi.js
@@ -3,7 +3,7 @@ const config = {
   apiKey: 'DX-Test_rGPk9D3wT9Ky2r4c_Yn7PulzV2UR-5C25aQ1byvuWtAByT3x3XnLm6njfmLa8NMo',
   endpoint: 'https://reportportal.dpl.usxpress.io/api/v1',
   project: 'default_personal',
-  launch: 'release-1.0',
+  launch: 'dev_geeks-for-geeks',
   // optional fields
   attributes: [
     {
@@ -16,7 +16,7 @@ const config = {
   ],
   description: 'This is an example launch with playwright tests',
   restClientConfig: {
-    timeout: 0,
+    timeout: 100,
   },
   includeTestSteps: true,
   skippedIssue: false,
