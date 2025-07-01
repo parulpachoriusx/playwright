54953feada02d2e7e066ca1e928ab742dcb68733
Parul Pachori <ppachori@usxpress.com>
2025-07-01 20:43:11 +0530

    test changes

--- rpConfigCi.js
+++ rpConfigCi.js
@@ -2,7 +2,7 @@ const config = {
   // required fields
   apiKey: 'testing_nG3vZILYSa6PfzsWQ1QHKEhoXEsjyF7j6Q7h4o6dw5f8BampCkR4id-iiDtNJfaa',
   endpoint: 'https://reportportal.usxpress.io/api/v1',
-  project: 'development',
+  project: 'superadmin_personal',
   launch: 'orders',
   // optional fields
   attributes: [
